82bed8a40b55cc11c53affb6e82775ae3690c290
Jay0007 <32128238+Jay0007@users.noreply.github.com>
2018-03-08 20:44:17 +0530

    Update index.vue

--- skylim/src/components/admission/index.vue
+++ skylim/src/components/admission/index.vue
@@ -1,40 +1,53 @@
 <template>
     <div id="app">
         <v-app id="inspire">
+            <v-card>
             <navbar class="mb-2"></navbar>
-            <v-stepper v-model="e1">
-                <v-stepper-header>
-                    <v-stepper-step step="1" :complete="e1 > 1">PERSONAL DETAILS</v-stepper-step>
-                    <v-divider></v-divider>
-                    <v-stepper-step step="2" :complete="e1 > 2">EDUCATIONAL BACKGROUND</v-stepper-step>
-                    <v-divider></v-divider>
-                    <v-stepper-step step="3">COURSE DETAILS</v-stepper-step>
-                    <v-divider></v-divider>
-                    <v-stepper-step step="4">FEE STRUCTURE</v-stepper-step>
-                </v-stepper-header>
-                
-                <v-stepper-items>
-                    <v-stepper-content step="1">
-                        <one :complete="e1"></one>
-                        <v-btn color="primary" @click.native="e1 = 2">Continue</v-btn>                        
-                    </v-stepper-content>
-                    <v-stepper-content step="2">
-                        <two></two>
-                        <v-btn color="primary" @click.native="e1 = 3">Continue</v-btn> 
-                        <v-btn color="primary" @click.native="e1 = 1">Go Back</v-btn>                        
-                       
-                    </v-stepper-content>
-                    <v-stepper-content step="3">
-                        <three></three>
-                        <v-btn color="primary" @click.native="e1 = 1">Continue</v-btn>
-                        <v-btn flat>Cancel</v-btn>
-                    </v-stepper-content>
-                </v-stepper-items>
 
-            </v-stepper>
+               
+                   <v-container>
+
+                   
+
+                    <v-stepper v-model="e1">
+                        <v-stepper-header>
+                            <v-stepper-step step="1" :complete="e1 > 1" editable></v-stepper-step>
+                            <v-divider></v-divider>
+                            <v-stepper-step step="2" :complete="e1 > 2" editable></v-stepper-step>
+                            <v-divider></v-divider>
+                            <v-stepper-step step="3" editable></v-stepper-step>
+                            <v-divider></v-divider>
+                            <v-stepper-step step="4" editable></v-stepper-step>
+                        </v-stepper-header>
+                        
+                        <v-stepper-items>
+                            <v-container>
+                            <v-stepper-content step="1">
+                                <one :complete="e1" @form1validity="btnStateChange"></one>
+                                <v-btn :disabled="canContinue" type="submit" color="primary"  @click.native="e1 = 2,submit" @click="one1">Continue</v-btn>                        
+                            </v-stepper-content>
+                            <v-stepper-content step="2">
+                                <v-flex>EDUCATIONAL BACKGROUND</v-flex>
+                                <two></two>
+                                <v-btn :disabled="errors.any()" type="submit" color="primary" @click.native="e1 = 3" @click="one2">Continue</v-btn>                        
+                                <v-btn color="primary" @click.native="e1 = 1">Go Back</v-btn>                        
+                            
+                            </v-stepper-content>
+                            <v-stepper-content step="3">
+                                <three></three>
+                                <v-btn color="primary" @click.native="e1 = 1">Continue</v-btn>
+                                <v-btn flat>Cancel</v-btn>
+                            </v-stepper-content>
+                            </v-container>
+                        </v-stepper-items>
+                
+                    
+                    </v-stepper>
+            </v-container>
+               </v-card>
         </v-app>
     </div>
-</template>
+</template>--->
   
 <script>
 
@@ -47,8 +60,37 @@ import three from './steps/three.vue'
 export default {
   data () {
     return {
-      e1: 0
+        e1: 0,
+        canContinue:true,
+        combine:[]
     }
+  },
+  computed: {
+        form(){
+            return this.$store.state.form;
+        },
+        form2(){
+            return this.$store.state.form2;
+        },
+  },
+  methods:{
+      one1:function(){
+          console.log(this.form)
+
+      },
+       one2:function(){
+           this.combine.push(this.form);
+           this.combine.push(this.form2);
+          console.log(this.combine)
+
+      },
+      submit () {
+        this.$validator.validateAll()
+      },
+      btnStateChange(event){
+          console.log(event);
+          this.canContinue = event;
+      }
   },
   components:{navbar,one,two,three}
 }
